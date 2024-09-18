fa35fdd94e5ccc2082ea3b86d1da66467e6e6eaa
yhw <425280162@qq.com>
2024-09-18 09:27:31 +0800

    fix

--- lottery-common/src/main/java/com/example/lottery/common/Constants.java
+++ lottery-common/src/main/java/com/example/lottery/common/Constants.java
@@ -9,6 +9,9 @@ package com.example.lottery.common;
  */
 public class Constants {
 
+    /**
+     * 枚举信息定义
+     */
     public enum ResponseCode {
         SUCCESS("0000", "成功"),
         UN_ERROR("0001", "未知失败"),
